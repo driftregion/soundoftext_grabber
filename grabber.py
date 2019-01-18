c71f36c03a4c41a536867892f3cb8ce4a986facd
Nick Kirkby <nkirkby@users.noreply.github.com>
2019-01-18 23:38:51 +0800

    removing CLI, switching to JSON phrase storage

--- grabber.py
+++ grabber.py
@@ -1,39 +1,68 @@
 #!/usr/bin/python3
 
 import requests
-import threading
-import argparse
 import os
 import time
+import json
+import logging
+import sys
 
 URL_BASE = "https://api.soundoftext.com/"
-DOWNLOADS_DIRECTORY = os.path.join(os.path.expanduser("~"), "Downloads")
+DOWNLOADS_DIRECTORY = os.path.join(os.path.dirname(os.path.abspath(__file__)), "sounds")
+PHRASES_JSON_NAME = "phrases.json"
+
+logging.basicConfig(stream=sys.stdout)
+logger = logging.getLogger()
+logger.setLevel(0)
 
 
 def get_request_body(text=""):
-    return {
-        "engine": "Google",
-        "data": {
-            "text": text,
-            "voice": "cmn-Hant-TW"
-        }
-    }
+    return {"engine": "Google", "data": {"text": text, "voice": "cmn-Hant-TW"}}
 
-def fetch(text=""):
-    print(f"fetching {text}")
+
+def fetch_phrase(text=""):
+    try:
+        os.mkdir(DOWNLOADS_DIRECTORY)
+    except:
+        pass
+    logger.info(f"fetching {text}", end=" ... ")
     body = get_request_body(text)
-    res = requests.post(URL_BASE + 'sounds/', json=body)
+    res = requests.post(URL_BASE + "sounds/", json=body)
     if res.status_code != 200:
         return
-    res = requests.get(URL_BASE + 'sounds/' + res.json()['id'])
-    time.sleep(1)    
-    res = requests.get(res.json()['location'])
-    with open(f'{DOWNLOADS_DIRECTORY}/{text}.mp3', 'wb') as sound:
+    res = requests.get(URL_BASE + "sounds/" + res.json()["id"])
+    time.sleep(1)
+    res = requests.get(res.json()["location"])
+    sound_path = f"{DOWNLOADS_DIRECTORY}/{text}.mp3"
+    with open(sound_path, "wb") as sound:
         sound.write(res.content)
-    print("done")
+    logger.info(f"saved to: {sound_path}")
+    return sound_path
 
-parser = argparse.ArgumentParser()
-parser.add_argument('text', nargs=1, default='', type=str, help="A string of words, space or comma separated, e.g. '中文 练习 下雪")
-args = parser.parse_args()
 
-fetch(args.text[0])
+def read_phrases_json():
+    with open(PHRASES_JSON_NAME, "r") as phrases_json_file:
+        phrases = json.load(phrases_json_file)
+    return phrases
+
+
+def write_phrases_json(phrases):
+    with open(PHRASES_JSON_NAME, "w") as phrases_json_file:
+        json.dump(phrases, phrases_json_file, ensure_ascii=False, indent=2)
+
+
+def download_phrases():
+    phrases = read_phrases_json()
+    for phrase in phrases:
+        if not phrase["en"]:
+            logger.error(f"no 'en' field for phrase {phrase}")
+            return
+        if phrase.get("path") and os.path.isfile(phrase.get("path")):
+            logger.debug(f"phrase {phrase['en']} has already been downloaded")
+        else:
+            sound_path = fetch_phrase(text=phrase["zh"])
+            phrase["path"] = sound_path
+        write_phrases_json(phrases)
+
+
+download_phrases()
---
+++ readme.md
@@ -0,0 +1,5 @@
+Automates the boring stuff related to memorising new phrases with Memrise.
+
+- English and Chinese pairs are stored in phrases.json
+- Chinese TTS soundbytes are generated using the soundoftext API, downloaded, and saved
+- new stuff is uploaded to memrise
